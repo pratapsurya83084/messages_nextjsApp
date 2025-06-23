81d292ef2b1358839681c4b39faeab48b42ca0b1
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2025-06-23 16:42:14 +0530

    nextjs fullstack project with otp auth and zod mesages

---
+++ src/models/User.ts
@@ -0,0 +1,75 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified:boolean,
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    trim: true,
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Please enter a valid email address",
+    ],
+    
+  },
+  password:{
+    type: String,
+    required: [true, "password is required"],
+    trim: true,
+
+  },
+  
+  verifyCode:{
+    type: String,
+    required: [true, "verifyCode is required"],
+   
+  },
+  verifyCodeExpiry:{
+    type: Date,
+    required: [true, "verifyCodeExpiry is required"],
+  },
+     isVerified:{
+    type:Boolean,
+    default:false,
+  },
+  isAcceptingMessage:{
+    type:Boolean,
+    default:true
+  },
+  messages: [MessageSchema],
+});
