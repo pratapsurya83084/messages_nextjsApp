5c8f40652389afe9c632c75d2d5e445af05ffd9c
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2025-06-26 00:49:50 +0530

    add  next-auth  and credential sign-in

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,90 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbconnect";
+import UserModel from "@/models/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifiers.email },
+              { username: credentials.identifiers.username },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before login");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            user.password,
+            credentials.password
+          );
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect password");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+          // console.error(error);
+        }
+      },
+    }),
+  ],
+
+
+ callbacks: {
+      async jwt({ token, user }) {
+      if (user) {
+           token._id =  user._id?.toString();
+           token.isVerified =  user.isVerified;
+           token.isAcceptingMessage =  user.isAcceptingMessage;
+           token.username =  user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+        if(token){
+            session.user._id = token?._id;
+            session.user.isVerified = token?.isVerified;
+            session.user.isAcceptingMessage = token?.isAcceptingMessage;
+            session.user.username = token?.username;
+
+
+        }
+      return session
+    },
+
+
+},
+
+
+  pages:{
+    signIn: '/sign-in',
+  },
+
+  session:{
+    strategy:"jwt",
+  },
+  secret:process.env.NEXTAUTH_SECRET
+
+
+};
--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -5,7 +5,6 @@ import { success } from "zod/v4";
 import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/helpers/sendVerification_email";
 
-
 export async function name(request: Request) {
   await dbConnect();
 
@@ -24,33 +23,78 @@ export async function name(request: Request) {
         message: "Username is already Existed",
       });
     }
-//check email is exists or not
-  const existingUserByEmail = await UserModel.findOne({email})
- //generate verifyCode
- const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
-  if (existingUserByEmail) {
-    true
-  }else{
- //then register user and save in db
- const hashedPassword = await bcrypt.hash(password, 10);
- const expiryDate = new Date();
- expiryDate.setDate(expiryDate.getDate() + 1);
-//save in db
- const newuser =  new UserModel({
-  username,
-  email,
- password: hashedPassword,
-  verifyCode,
-  verifyCodeExpiry: expiryDate,
-  isVerified:false,
-  isAcceptingMessage: true,
-  messages: []
- })
+    //check email is exists or not
+    const existingUserByEmail = await UserModel.findOne({ email });
+    //generate verifyCode
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    
+    if (existingUserByEmail) {
 
- await newuser.save();
-}
-await sendVerificationEmail(email,username , verifyCode);
+    if (existingUserByEmail.isVerified) {
+     return Response.json(
+        {
+          success: false,
+          message: "User already exists",
+        },
+        { status: 400 }
+      );
+    }else{
+      //user is not verified then 
+      const hasedPassword = await bcrypt.hash(password,10);
+       existingUserByEmail.password = hasedPassword;
+       existingUserByEmail.verifyCode = verifyCode;
+       existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+       existingUserByEmail.save();
+       return Response.json({
+        success: true,
+        message: "User already exists, please verify your email",
+        });
 
+    }
+
+    } else {
+      //then register user and save in db
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setDate(expiryDate.getDate() + 1);
+      //save in db
+      const newuser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newuser.save();
+    }
+    //send verification email
+    const emaILResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emaILResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emaILResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User  registered successfully ,Please verify your email",
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error registering User ", error);
     return Response.json(
