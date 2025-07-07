c217f8dbb64cfa1160c8b5f3db84114ab53bba78
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2025-07-08 00:01:24 +0530

    add dashboard ui

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -12,11 +12,20 @@ import axios, { AxiosError } from "axios";
 import { ApiError } from "next/dist/server/api-utils";
 import { ApiResponse } from "@/types/Apiresponse";
 import { User } from "next-auth";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { Separator } from "@/components/ui/separator";
+import { Loader2, RefreshCcw } from "lucide-react";
+import MessageCard from '@/components/MessageCard'
 
 const page = () => {
   const [message, setMessage] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isSwitchingLoading, setIsSwitchingLoading] = useState(false);
+
+
+console.log('message fetching :',message);
+
 
 const handleDeleteMessage = (messageId: string) => {
   setMessage(message.filter((msg) => msg._id !== messageId));
@@ -70,7 +79,7 @@ const fetchMessages = useCallback(async (refresh: boolean = false) => {
     const response = await axios.get<ApiResponse>('/api/get-messages');
 
     // Assuming you want to do something with the data:
-    // setMessages(response.data.messages); 
+    setMessage(response.data.messages??[]); 
 
     setIsLoading(false);
 if (refresh) {
@@ -133,16 +142,18 @@ try {
 
 //return user url and copy button 
 
-const {username} = session?.user as User
+const user = session?.user as User  
+const username = user?.username
+console.log("username is : " ,user?.username);
 
 const baseUrl = `${window.location.protocol}//${window.location.host}`;
 console.log(baseUrl);
 
-const profiUrl = `${baseUrl}/u/${username}`
+const profileUrl = `${baseUrl}/u/${username}`
 
 
 const copyToClipboard = () =>{
-navigator.clipboard.writeText(profiUrl);
+navigator.clipboard.writeText(profileUrl);
 toast("success",{
 description:"Profile Url has beem copied to Clipboard"
 })
@@ -157,7 +168,64 @@ if (!session || !session.user) {
 
 
 
-  return <div>Dashboard</div>;
+  return (
+<div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+  <h1 className="text-4xl font-bold mb-4"> User Dashboard </h1>
+
+  <div className="mb-4">
+<h2 className="text-4xl font-bold mb-4"> Copy Your unique Link </h2>
+
+ <div className="flex items-center">
+<input type="text"  value={profileUrl} 
+className="input input-bordered w-full  p-2 mr-2"
+disabled 
+id="" />
+<Button onClick={copyToClipboard}>Copy</Button>
+ </div>
+ 
+  </div>
+
+<div className="mb-4">
+  
+<Switch
+{...register('acceptMessage')}
+  checked={acceptMessages}
+  onCheckedChange={handelSwitchChange}
+  disabled={isSwitchingLoading}
+/>
+
+<span className="ml-2">
+Accept Messages : {acceptMessages?"On":"Off"}
+</span>
+</div>
+<Separator/>
+<Button className="mt-4"
+variant={'outline'}
+onClick={(e)=>{e.preventDefault()
+  fetchMessages(true)
+  }}>
+{isLoading?(
+<Loader2 className="h-4 w-4 animate-spin"/>
+) :(
+  <RefreshCcw className="h-4 w-4"/>
+)} 
+   </Button>
+    <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {message.length > 0 ? (
+          message.map((messages, index) => (
+            <MessageCard
+              key={index}     // messages
+              message={messages}
+              onMessageDelete={handleDeleteMessage}
+            />
+          ))
+        ) : (
+          <p>No messages to display.</p>
+        )}
+      </div>
+</div>
+
+  )
 };
 
 export default page;
